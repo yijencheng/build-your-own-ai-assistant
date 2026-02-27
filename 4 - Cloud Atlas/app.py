1f1cf26ede437aee5ced19c87205fb704c8961b5
Ivan Leo <ivanleomk@gmail.com>
2026-02-27 15:38:29 +0800

    fix: add sandbox

--- 4 - Cloud Atlas/app.py
+++ 4 - Cloud Atlas/app.py
@@ -3,6 +3,8 @@ import queue
 
 from telegram_types import TelegramUpdate
 
+SCALEDOWN_WINDOW = 300
+
 image = (
     modal.Image.debian_slim()
     .pip_install("pydantic>=2,<3", "fastapi[standard]>=0.115,<1")
@@ -16,7 +18,7 @@ snapshot_dict = modal.Dict.from_name("koroku-snapshots", create_if_missing=True)
 message_queue = modal.Queue.from_name("koroku-messages", create_if_missing=True)
 
 
-@app.cls(scaledown_window=300, timeout=3600)
+@app.cls(scaledown_window=SCALEDOWN_WINDOW, timeout=SCALEDOWN_WINDOW)
 class WebApp:
     """
     Webapp here stays alive for 5 minutes
@@ -25,24 +27,24 @@ class WebApp:
     @modal.enter()
     def startup(self):
         print("STARTING")
-        # snapshot_id = snapshot_dict.get("SNAPSHOT", None)
-        # if snapshot_id:
-        #     print(f"Restoring sandbox from snapshot: {snapshot_id}")
-        #     snapshot = modal.SandboxSnapshot.from_id(snapshot_id)
-        #     self.sandbox = modal.Sandbox._experimental_from_snapshot(snapshot)
-        # else:
-        #     print("Creating new sandbox")
-        #     self.sandbox = modal.Sandbox.create(
-        #         app=sandbox_app, image=sandbox_image, _experimental_enable_snapshot=True
-        #     )
+        snapshot_id = snapshot_dict.get("SNAPSHOT", None)
+        if snapshot_id:
+            print(f"Restoring sandbox from snapshot: {snapshot_id}")
+            snapshot = modal.SandboxSnapshot.from_id(snapshot_id)
+            self.sandbox = modal.Sandbox._experimental_from_snapshot(snapshot)
+        else:
+            print("Creating new sandbox")
+            self.sandbox = modal.Sandbox.create(
+                app=sandbox_app, image=sandbox_image, timeout=SCALEDOWN_WINDOW, _experimental_enable_snapshot=True
+            )
 
     @modal.exit()
     def shutdown(self):
         print("SHUTTING DOWN")
-        # print("Snapshotting sandbox...")
-        # snapshot = self.sandbox._experimental_snapshot()
-        # snapshot_dict["SNAPSHOT"] = snapshot.object_id
-        # print(f"Saved snapshot: {snapshot.object_id}")
+        print("Snapshotting sandbox...")
+        snapshot = self.sandbox._experimental_snapshot()
+        snapshot_dict["SNAPSHOT"] = snapshot.object_id
+        print(f"Saved snapshot: {snapshot.object_id}")
 
     @modal.fastapi_endpoint(method="POST", docs=True)
     async def chat(self, update: TelegramUpdate):
